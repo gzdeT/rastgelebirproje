57fa6912e612f43b6fa71650989093fd54867ec5
gzdeT <gozde.tursun@demilac.com.tr>
2018-03-07 17:38:47 +0300

    deneme

---
+++ deneme/rastgelebirproje1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rastgelebirproje1.Startup))]
+namespace rastgelebirproje1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
